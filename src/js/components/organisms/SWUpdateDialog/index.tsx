89c3a202e731a8bc05071927e3d4acc1c737bd14
aikatsukamen <aikatsukamen@gmail.com>
2022-04-20 02:19:38 +0900

    ccc

--- src/js/components/organisms/SWUpdateDialog/index.tsx
+++ src/js/components/organisms/SWUpdateDialog/index.tsx
@@ -4,8 +4,19 @@ export const SWUpdateDialog: React.FC<{ registration: ServiceWorkerRegistration 
   const [show, setShow] = useState(!!registration.waiting);
   const style: React.CSSProperties = {
     width: '100%',
-    background: 'linear-gradient(180deg, #fe8ad1 0%, #eaa1cd 20%, #7fd6fd 70%, #a4f1e8 100%)',
+    backgroundImage: 'linear-gradient(rgb(255, 255, 255), rgb(255, 250, 222))',
+    color: 'black',
+    padding: 5,
   };
+  const buttonStyle: React.CSSProperties = {
+    backgroundColor: 'ff6aac',
+    color: 'fff',
+    boxShadow: '0 6px 16px rgb(255 106 172 / 50%)',
+    fontWeight: 600,
+    borderRadius: 3,
+    height: 30,
+  };
+
   const handleUpdate = () => {
     registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
     setShow(false);
@@ -14,8 +25,10 @@ export const SWUpdateDialog: React.FC<{ registration: ServiceWorkerRegistration 
 
   return show ? (
     <div style={style}>
-      <span>更新があります</span>
-      <button onClick={handleUpdate}>アップデート</button>
+      <span style={{ marginRight: 15 }}>アプリに更新があります</span>
+      <button style={buttonStyle} onClick={handleUpdate}>
+        アップデート
+      </button>
     </div>
   ) : (
     <></>
